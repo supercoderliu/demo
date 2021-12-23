33b16e7295d6170e00433811653175901197a540
liuliang <liuliang@yiyouliao.com>
2021-12-23 14:37:36 +0800

    add gateway

--- demo-gateway/src/main/java/com/example/demogateway/DemoGatewayApplication.java
+++ demo-gateway/src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.demogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoGatewayApplication.class, args);
     }
 
 }
