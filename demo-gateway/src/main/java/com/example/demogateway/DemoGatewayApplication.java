bcb706363860d3886b401424ae9bbcae05b79bf0
liuliang <liuliang@yiyouliao.com>
2021-12-24 14:31:11 +0800

    add gateway

--- demo-gateway/src/main/java/com/example/demogateway/DemoGatewayApplication.java
+++ demo-gateway/src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -9,5 +9,13 @@ public class DemoGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoGatewayApplication.class, args);
     }
-
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p->p
+//                        .path("/get")
+//                        .filters(f->f.addRequestHeader("hello","world"))
+//                        .uri("http://httpbin.org:80"))
+//                .build();
+//    }
 }
---
+++ demo-gateway/src/main/java/com/example/demogateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.demogateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @program:
+ * @description:
+ * @author: Liu
+ * @create: 2021-12-24 11:45
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
